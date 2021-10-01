2e51c936c06fe7304102e49b5d7bfb637a338616
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2021-10-01 20:35:34 -0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void mostrarOla()
+        {
+            Console.WriteLine("Olá - estou na função MostrarOla()");
+        }
+
+        static int dobro(int n)
+        {
+            return (n * 2);
+        }
+
+        static int areaRetangulo(int b, int h)
+        {
+            int x;
+            x = b * h;
+            return x;
+        }
+
+        static void Main(string[] args)
+        {
+            int p;
+
+            Console.WriteLine("Estou na função Main()");
+            mostrarOla();
+            Console.WriteLine("Estou novamente na função Main()");
+            Console.WriteLine("O dobro de 3 é {0}", dobro(3));
+            mostrarOla();
+            Console.WriteLine("A área de um retângulo com base 4 e altura 5 é {0}", areaRetangulo(4, 5));
+            mostrarOla();
+
+            Console.Write("Digite um número qualquer: ");
+            p = int.Parse(Console.ReadLine());
+            if (dobro(p) > 20)
+                Console.WriteLine("O dobro de {0} é maior do que 20", p);
+            else
+                Console.WriteLine("O dobro de {0} não é maior do que 20", p);
+        }
+    }
+}
---
+++ projSoma/projSoma/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projSoma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            int n2;
+
+            n1 = entrarInt("Digite o valor de n1: ", 5, 10);
+            n2 = entrarInt("Digite o valor de n2: ", 10, 20);
+
+            Console.WriteLine("Resultado da soma de {0} com {1} é {2}", n1, n2, calcular(n1, n2));
+        }
+
+        static int entrarInt(string msg, int li, int ls)
+        {
+            int x;
+            do
+            {
+               Console.Write(msg);
+               x = int.Parse(Console.ReadLine());
+            }
+            while (x < li || x > ls);
+            return x;
+        }
+
+        static int calcular(int x, int y)
+        {
+            return (x + y);
+        }
+    }
+}
